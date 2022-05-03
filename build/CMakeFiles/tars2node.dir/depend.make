f8bc953161c170c2feffafe03e610036d8f81b6d
zouchengzhuo <405966530@qq.com>
2022-05-03 05:54:56 +0000

    添加调试信息

---
+++ build/CMakeFiles/tars2node.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tars2node.
+# This may be replaced when dependencies are built.
--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,7 @@ void check(vector<string> &vFiles)
 
 int main(int argc, char* argv[])
 {
+    std::cout << " [DEBUG] argc:" << argc << std::endl;
     if(argc < 2)
     {
         usage();
